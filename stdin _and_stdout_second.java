bee99b2a416fa15250ae533874ae05ca13663819
Ashutosh Das <155134094+ashutoshdas1337@users.noreply.github.com>
2024-07-10 19:08:42 +0530

    Update and rename stdin and stdout 2 to stdin _and_stdout_second.java
    solution of input output problem 2

--- stdin _and_stdout_second.java
+++ stdin _and_stdout_second.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class stdin_and_stdout_second{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
